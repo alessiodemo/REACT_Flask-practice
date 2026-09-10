091a08b7531a58ae57e7d096211d71fafce97e9c
Alessio Demo <alessiodemo@gmail.com>
2026-09-10 23:52:52 +0200

    added fetch movie function from API

--- movie_react/frontend/src/pages/Home.jsx
+++ movie_react/frontend/src/pages/Home.jsx
@@ -1,17 +1,29 @@
 import MovieCard from "../components/MovieCard"
-import {useState} from "react"
+import {useState, useEffect, use} from "react"
+import {searchMovies, getPopularMovies} from "../services/api"
 import "../css/Home.css"
 
 function Home () {
 
     const [ searchQuery, setSearchQuery ] = useState("")
+    const [ movies, setMovies ] = useState([])
+    const [ error, setError ] = useState(null)
+    const [ loading, setLoading ] = useState(true)
     
-    const movies = [
-        {id: 1, title: "John Wick", release_date: "2020"},
-        {id: 2, title: "Terminator", release_date: "1999"},
-        {id: 3, title: "The Matrix", release_date: "1998"},
-        {id: 4, title: "Pep Guardiola", release_date: "2022"},
-    ];
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } catch (err) {
+                console.log(err)
+                setError("Failed to load movies...")
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+    }, [])
 
     const handleSearch = (e) => {
         e.preventDefault()
---
+++ movie_react/frontend/src/services/api.js
@@ -0,0 +1,20 @@
+const API_KEY = "a847ac83a30ed63b540ac3fc82859931"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+            query
+        )}`
+    );
+    const data = await response.json()
+    return data.results
+};
+
