d487af05e9a7a00309e1a080df3b115d1d4f0916
Alessio Demo <alessiodemo@gmail.com>
2026-09-13 13:14:57 +0200

    introduced search features and first stages of the favorites feature

--- movie_react/frontend/src/pages/Home.jsx
+++ movie_react/frontend/src/pages/Home.jsx
@@ -25,32 +25,53 @@ function Home () {
         }
     }, [])
 
-    const handleSearch = (e) => {
+    const handleSearch = async (e) => {
         e.preventDefault()
+
+        if (!searchQuery.trim()) return
+        if (loading) return
+
+        setLoading(true)
+        try {
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        } catch (err) {
+            console.log(err)
+            setError("Failed to search movies...")
+        } finally {
+            setLoading(false)
+        }
+
+        setLoading(true)
         alert(searchQuery)
         setSearchQuery("")
     };
 
-    return <div className="home">
-        <form onSubmit={handleSearch} className="search-form">
-            <input 
-                type="text" 
-                placeholder="Search for movies..." 
-                className="search-input"
-                value = { searchQuery }
-                onChange = {(e) => setSearchQuery(e.target.value)}
-            />
-            <button type="submit" className="search-button">Search</button>
-        </form>
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input 
+                    type="text" 
+                    placeholder="Search for movies..." 
+                    className="search-input"
+                    value = { searchQuery }
+                    onChange = {(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
 
-        <div className="movies-grid">
-            {movies.map(movie => (
-                (
-                <MovieCard movie={movie} key={movie.id}/>
-                )
-            ))}
+            { error && <div className="error-message">{error}</div>}
+
+            { loading ? <div className="loading">Loading...</div> : <div className="movies-grid">
+                {movies.map(movie => (
+                    (
+                    <MovieCard movie={movie} key={movie.id}/>
+                    )
+                ))}
+            </div>}
         </div>
-    </div>
+    )
 }
 
 export default Home
